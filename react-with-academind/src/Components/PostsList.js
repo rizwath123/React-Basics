844820fbe2fde83e3c05cc08d66eb2c6c710f28b
rizwath123 <rizwathr@gmail.com>
2023-02-23 23:44:52 +0300

    create close modal clicking outside added

--- react-with-academind/src/Components/PostsList.js
+++ react-with-academind/src/Components/PostsList.js
@@ -2,23 +2,46 @@ import styles from "./PostsList.module.css"
 import { Post } from "./Post"
 import { CreatePost } from "./CreatePost"
 import { useState } from 'react'
-
+import Modal from "./Modal"
 
 export function PostsList() {
     const [enteredBody, setEnteredBody] = useState('')
     const [enteredName, setEnteredName] = useState('')
+    const [isModalOpen, setISModalOpen] = useState(true)
+    function hideModal() {
+        setISModalOpen(false)
+    }
+
     function bodyChangeHandler(event) {
         setEnteredBody(event.target.value)
     }
     function onNameChangeHandler(event) {
         setEnteredName(event.target.value)
     }
+    // let modelVisible;
 
-
+    // if (isModalOpen) {
+    //     modelVisible = <Modal onClose={hideModal}>
+    //         <CreatePost
+    //             onBodyChange={bodyChangeHandler}
+    //             onNameChange={onNameChangeHandler} />
+    //     </Modal>
+    // }
     return (
         <div>
-            <CreatePost onBodyChange={bodyChangeHandler}
-                onNameChange={onNameChangeHandler} />
+            {isModalOpen && (
+                <Modal onClose={hideModal}>
+                <CreatePost
+                    onBodyChange={bodyChangeHandler}
+                    onNameChange={onNameChangeHandler} />
+            </Modal>
+            )}
+            {/* {isModalOpen ? <Modal onClose={hideModal}>
+                <CreatePost
+                    onBodyChange={bodyChangeHandler}
+                    onNameChange={onNameChangeHandler} />
+            </Modal> : null} */}
+
             <ul className={styles.posts}>
                 <Post name={enteredName} text={enteredBody} />
                 <Post name="ReactJs" text="I have done it" />
