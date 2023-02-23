37ad99563d62137c8966cea2da6e8f52e6ec436e
rizwath123 <rizwathr@gmail.com>
2023-02-23 22:58:21 +0300

    create post listed in postlist completed

---
+++ react-with-academind/src/Components/CreatePost.js
@@ -0,0 +1,30 @@
+// import {useState} from 'react'
+import classes from "./CreatePost.module.css"
+
+
+export function CreatePost(props) {
+    // const [enteredBody, setEnteredBody] = useState('')
+    // let enteredBody = '';
+    // function onChangeEventHandler(event){
+    //     setEnteredBody(event.target.value);
+    // }
+
+    return (
+
+        <form className={classes.form}>
+            <p>
+                <label  htmlFor="body">Your Message:</label>
+                <textarea  type="text" id="body" required rows={3}
+                    placeholder="Type your message here" onChange={props.onBodyChange}></textarea>
+            </p>
+            {/* <p>
+                {enteredBody}
+            </p> */}
+            <p>
+                <label htmlFor="name">Your Name:</label>
+                <input  id="name" type="text" required 
+                    placeholder="Type your name here"></input>
+            </p>
+        </form>
+    )
+}
--- react-with-academind/src/Components/PostsList.js
+++ react-with-academind/src/Components/PostsList.js
@@ -1,12 +1,19 @@
 import styles from "./PostsList.module.css"
 import { Post } from "./Post"
 import { CreatePost } from "./CreatePost"
+import {useState} from 'react'
+
+
 export function PostsList() {
+    const [enteredBody, setEnteredBody] = useState('')
+    function bodyChangeHandler(event){
+        setEnteredBody(event.target.value)
+    }
     return (
         <div>
-            <CreatePost/>
+            <CreatePost onBodyChange={bodyChangeHandler}/>
             <ul className={styles.posts}>
-                <Post name="Riz" text="this is how props works" />
+                <Post name="Riz" text={enteredBody} />
                 <Post name="ReactJs" text="I have done it" />
                 <Post name="React State" text="Lets learn React State Next" />
             </ul>
