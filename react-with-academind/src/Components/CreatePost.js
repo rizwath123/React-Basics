22fc9afac87cb462cc3c2e4334f87aa3dd878585
rizwath123 <rizwathr@gmail.com>
2023-02-25 17:15:07 +0300

    creating posts and displaying it executed

--- react-with-academind/src/Components/CreatePost.js
+++ react-with-academind/src/Components/CreatePost.js
@@ -1,34 +1,50 @@
-// import {useState} from 'react'
+import {useState} from 'react'
 import classes from "./CreatePost.module.css"
 
 
 export function CreatePost(props) {
-    // const [enteredBody, setEnteredBody] = useState('')
-    // let enteredBody = '';
-    // function onChangeEventHandler(event){
-    //     setEnteredBody(event.target.value);
-    // }
-
+    const [enteredBody, setEnteredBody] = useState('')
+    const [enteredName, setEnteredName] = useState('')
+    
+    function bodyChangeHandler(event) {
+        setEnteredBody(event.target.value)
+    }
+    function onNameChangeHandler(event) {
+        setEnteredName(event.target.value)
+    }
+   function submitHandlerFn(event){
+    event.preventDefault()
+    const postData = {
+        body: enteredBody,
+        name: enteredName
+    }
+    props.onAddPost(postData)
+    console.log(postData);
+    props.closeFn()
+   }
     return (
 
-        <form className={classes.form}>
+        <form className={classes.form} onSubmit={submitHandlerFn}>
             <p>
-                <label  htmlFor="body">Your Message:</label>
-                <textarea  type="text" id="body" required rows={3}
-                    placeholder="Type your message here" onChange={props.onBodyChange}></textarea>
+                <label htmlFor="body">Your Message:</label>
+                <textarea type="text" id="body" required rows={3}
+                    placeholder="Type your message here" onChange={bodyChangeHandler}></textarea>
             </p>
             {/* <p>
                 {enteredBody}
+                
+                    onBodyChange={bodyChangeHandler}
+                    onNameChange={onNameChangeHandler} 
             </p> */}
             <p>
                 <label htmlFor="name">Your Name:</label>
-                <input  id="name" type="text" required 
-                    placeholder="Type your name here" onChange={props.onNameChange}></input>
+                <input id="name" type="text" required
+                    placeholder="Type your name here" onChange={onNameChangeHandler}></input>
             </p>
             <p className={classes.actions}>
 
-            <button className={classes.button} type="button" onClick={props.closeFn}>Cancel</button>
-            <button className={classes.button} type="submit">Submit</button>
+                <button className={classes.button} type="button" onClick={props.closeFn}>Cancel</button>
+                <button className={classes.button} type="submit">Submit</button>
             </p>
         </form>
     )
--- react-with-academind/src/Components/PostsList.js
+++ react-with-academind/src/Components/PostsList.js
@@ -4,20 +4,34 @@ import { CreatePost } from "./CreatePost"
 import { useState } from 'react'
 import Modal from "./Modal"
 
-export function PostsList({isPosting,onStopPosting}) {
-    const [enteredBody, setEnteredBody] = useState('')
-    const [enteredName, setEnteredName] = useState('')
-    
+export function PostsList({ isPosting, onStopPosting }) {
+    const [posts, setPosts] = useState([])
 
+    function addPostHandlerFn(postData) {
+        setPosts((existingPosts) => [postData, ...existingPosts])
+    }
+    return (
+        <div>
+            {isPosting && (
+                <Modal onClose={onStopPosting}>
+                    <CreatePost
+                        closeFn={onStopPosting} onAddPost={addPostHandlerFn} />
+                </Modal>
+            )}
+            {/* {isModalOpen ? <Modal onClose={hideModal}>
+                <CreatePost
+                    onBodyChange={bodyChangeHandler}
+                    onNameChange={onNameChangeHandler} />
+            </Modal> : null} */}
+            {posts.length > 0 && (<ul className={styles.posts}>
+                {posts.map((data) => <Post key={data.name} name={data.name} text={data.body} />)}
 
-    function bodyChangeHandler(event) {
-        setEnteredBody(event.target.value)
-    }
-    function onNameChangeHandler(event) {
-        setEnteredName(event.target.value)
-    }
+            </ul>)}
+            {posts.length === 0 && (<div style={{ textAlign: "center", backGround: "Red", color: "yellow" }}>
+                <h1>There are No posts to show</h1></div>)}
+        </div>
+    )
     // let modelVisible;
-
     // if (isModalOpen) {
     //     modelVisible = <Modal onClose={hideModal}>
     //         <CreatePost
@@ -25,27 +39,4 @@ export function PostsList({isPosting,onStopPosting}) {
     //             onNameChange={onNameChangeHandler} />
     //     </Modal>
     // }
-    return (
-        <div>
-            {isPosting && (
-                <Modal onClose={onStopPosting}>
-                <CreatePost
-                    onBodyChange={bodyChangeHandler}
-                    onNameChange={onNameChangeHandler} 
-                    closeFn={onStopPosting}/>
-            </Modal>
-            )}
-            {/* {isModalOpen ? <Modal onClose={hideModal}>
-                <CreatePost
-                    onBodyChange={bodyChangeHandler}
-                    onNameChange={onNameChangeHandler} />
-            </Modal> : null} */}
-
-            <ul className={styles.posts}>
-                <Post name={enteredName} text={enteredBody} />
-                <Post name="ReactJs" text="I have done it" />
-                <Post name="React State" text="Lets learn React State Next" />
-            </ul>
-        </div>
-    )
 }
